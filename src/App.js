28d39400b980b66204dbfc2d9daed4949f9e00b9
Fernando Resendiz <fernando.resendiz@sonatasmx.com>
2020-02-04 17:20:07 -0600

    First commit - project structure

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div className="spinner">
+            <div className="bounce1"></div>
+            <div className="bounce2"></div>
+            <div className="bounce3"></div>        
+        </div>        
+    );
+  }
+  
+  export default App;
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      },
+      { 
+        test: /\.css$/, 
+        use: [
+          MiniCssExtractPlugin.loader, 
+          "css-loader"
+        ] 
+      }      
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    }),
+    new MiniCssExtractPlugin()
+  ]
+};
