df8d2506c2d5be734e0847759c48e9eba11e6480
Fernando Resendiz <fernando.resendiz@sonatasmx.com>
2020-02-10 15:14:47 -0600

    Added more info records for Patient Details screen

--- src/js/components/container/details.js
+++ src/js/components/container/details.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { faChevronLeft, faUser } from "@fortawesome/free-solid-svg-icons";
 
 import { baseUrl, REQUEST_DELAY } from "./../../constants";
 import { history } from "./../../helpers";
@@ -61,30 +61,89 @@ export default class Details extends Component {
 
     renderDetails() {
         /*<div>
-            <div className="panel panel-default">
-                <div className="panel-body">
-                    This is a Basic panel
-                </div>
-            </div>                
+                <div class="panel panel-default">
+                    <div class="panel-body">A Basic Panel</div>
+                </div>             
         </div>*/
+        let details = this.state.details;
+
         return (
-            <table className="table">
-                <tbody>
-                    <tr>
-                        <td>First Name</td>
-                        <td className="dark text-right"><b>{this.state.details.name[0].given[0]}</b></td>
-                    </tr>
-                    <tr>
-                        <td>Last Name</td>
-                        <td className="dark text-right"><b>{this.state.details.name[0].family}</b></td>
-                    </tr>                    
-                </tbody>                
-            </table>
+            <div className="container">
+                <table className="table table-hover">
+                    <tbody>
+                        <tr>
+                            <td>First Name</td>
+                            <td className="dark text-right"><b>{details.name[0].given[0]}</b></td>
+                        </tr>
+                        <tr>
+                            <td>Last Name</td>
+                            <td className="dark text-right"><b>{details.name[0].family}</b></td>
+                        </tr> 
+                        <tr>
+                            <td>Prefix</td>
+                            <td className="dark text-right"><b>{!!details.name[0].prefix ? details.name[0].prefix[0] : ''}</b></td>
+                        </tr>
+                        <tr>
+                            <td>Gender</td>
+                            <td className="dark text-right"><b>{details.gender}</b></td>
+                        </tr>
+                        <tr>
+                            <td>Birth Date</td>
+                            <td className="dark text-right"><b>{details.birthDate}</b></td>
+                        </tr>                                                                                       
+                    </tbody>                
+                </table>
+                <h2>Address</h2>         
+                <table className="table table-hover">
+                    <tbody>
+                        <tr>
+                            <td>Country</td>
+                            <td className="dark text-right"><b>{details.address[0].country}</b></td>
+                        </tr> 
+                        <tr>
+                            <td>State</td>
+                            <td className="dark text-right"><b>{details.address[0].state}</b></td>
+                        </tr>
+                        <tr>
+                            <td>City</td>
+                            <td className="dark text-right"><b>{details.address[0].city}</b></td>
+                        </tr>
+                        <tr>
+                            <td>Street</td>
+                            <td className="dark text-right"><b>{details.address[0].line[0]}</b></td>
+                        </tr>
+                        <tr>
+                            <td>Zip Code</td>
+                            <td className="dark text-right"><b>{details.address[0].postalCode}</b></td>
+                        </tr>                                                
+                    </tbody>                
+                </table>
+                <h2>Contact</h2>         
+                <table className="table table-hover">
+                    <tbody>
+                        <tr>
+                            <td>Phone</td>
+                            <td className="dark text-right"><b>{!!details.telecom[0] ? details.telecom[0].value : ''}</b></td>
+                        </tr> 
+                        <tr>
+                            <td>E-mail</td>
+                            <td className="dark text-right"><b>{!!details.telecom[1] ? details.telecom[1].value : ''}</b></td>
+                        </tr>                                             
+                    </tbody>                
+                </table>
+                <h1></h1>
+                <table>
+                    <thead style={{ cursor: 'pointer'}} onClick={ e => history.push(`${baseUrl}/`) }>
+                        <tr>
+                            <th className="text-right"><FontAwesomeIcon icon={faChevronLeft} />&nbsp;&nbsp;&nbsp;Back to Patients List</th>
+                        </tr>
+                    </thead>
+                </table>                
+            </div>          
         );
     }    
 
-    render() {
-        console.log("details ", this.state.details)
+    render() {        
         return (
             <div>
                 <div>
