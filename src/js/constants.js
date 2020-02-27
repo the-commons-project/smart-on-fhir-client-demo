4ebd3d5e915409aa5111ebe334aa9201ff6ac518
Fernando Resendiz <fernando.resendiz@sonatasmx.com>
2020-02-27 08:59:04 -0600

    Updated fhir api endpoint to url pointing ec2 instance created

--- src/js/constants.js
+++ src/js/constants.js
@@ -1,6 +1,7 @@
 export const PATH_DETAILS = "details";
 
-export const API_URL = "https://r3.smarthealthit.org";
+//export const API_URL = "https://r3.smarthealthit.org";
+export const API_URL= "http://fhir-client-demo.tcpdev.org:4003/baseDstu3";
 
 export const REQUEST_DELAY = 1000;
 
